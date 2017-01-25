58c4622ea5446899b0c0e0d79d0a45527a47214d
Friedrich Wessel <mail@friedrichwessel.de>
2017-01-25 21:20:32 +0100

    Add Port as cmdline arg

--- go/simpleHTTPServer.go
+++ go/simpleHTTPServer.go
@@ -2,14 +2,18 @@ package main
 
 import "net/http"
 import "fmt"
+import "flag"
 
 func main(){
+  portPtr := flag.String("port", "10800", "port to serve")
+  flag.Parse()
   http.HandleFunc("/", serve)
-  http.ListenAndServe(":8081", nil)
+  fmt.Printf("Server is running on port: " + *portPtr +"\n")
+  http.ListenAndServe(":"+*portPtr, nil)
 }
 
 func serve(response http.ResponseWriter, request *http.Request){
   path := request.URL.Path[1:]
-  fmt.Printf("Serving path: " + path)
+  fmt.Printf("Serving path: " + path + "\n")
   http.ServeFile(response, request, request.URL.Path[1:])
 }
